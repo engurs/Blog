60b03a7621165a9ba96e5b6d246febc923cb03cd
sezn <sezinengur@gmail.com>
2017-03-24 16:27:03 +0300

    Veritabanındaki postları görebilmek için template'i değiştirdim.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""from django.shortcuts import render
+from django.utils import timezone
+from .models import Post #models.py'de yazdigimiz modeli ekleme
+#fromdan sonraki nokta mevcut dizin veya mevcut uygulama anlamna geliyor. views.py ve models.py ayni dizinde olduklari icin sadece "."
 
 # Create your views here.
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
+def post_list(request):
+	posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')     
+    # yaynlanms ve yayinlanma_tarihine gore sralanms bir gönderi listesi
+    #QuerySet imiz icin bir degisken yarattigimiza dikkat edin: posts. Bu QuerySetin ismi.
+    return render(request, 'blog/post_list.html', {'posts': posts})"""
+
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
